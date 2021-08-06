56fe0c24499fb6562bc45ad26389a4d0d5ce8330
finite-automaton <71351488+finite-automaton@users.noreply.github.com>
2021-08-06 16:13:08 +0100

    added our database model

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from models import (Base, session,
+                    Book, engine)
+
+if __name__ == '__main__':
+  Base.metadata.create_all(engine)
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to the database
